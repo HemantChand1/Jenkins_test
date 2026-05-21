33aa1a09e72dccd9047115ad188560fea25ff7fb
HemantChand1 <hchandraj11@gmail.com>
2026-05-21 08:34:36 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("  Hello from Jenkins Pipeline! ");
+        System.out.println("  Build Successful!            ");
+        System.out.println("================================");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
